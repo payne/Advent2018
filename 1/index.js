94e7d3f2832a01608ab22adc3b1cf38b2ff8bdaf
Matt Payne <Payne@MattPayne.org>
2018-12-02 13:52:46 -0600

    more hipster.

--- 1/index.js
+++ 1/index.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 
 fs.readFile("input.txt", "utf8", function(err, data) {
   if (err) throw err;
-  const answer = data.split("\n").reduce((a, v) => a + 1 * v, 0);
-  console.log(answer);
+  console.log(`Resulting frequency: ${resultingFrequency(data)}`);
 });
+
+resultingFrequency = data => data.split("\n").reduce((a, v) => a + 1 * v, 0);
