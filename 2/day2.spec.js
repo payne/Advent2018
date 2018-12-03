e52c129001a82726f76a3b308e777be5af1dd46c
Matt Payne <Payne@MattPayne.org>
2018-12-02 21:06:45 -0600

    npm test #fails -- see the WHY comment.

--- 2/day2.spec.js
+++ 2/day2.spec.js
@@ -49,7 +49,42 @@ const exampleInput = [
   "ababab"
 ];
 
+const example2Input = [
+  "abcde",
+  "fghij",
+  "klmno",
+  "pqrst",
+  "fguij",
+  "axcye",
+  "wvxyz"
+];
+
+diffPositions = (s1, s2) => {
+  const a1 = s1.split("");
+  const a2 = s2.split("");
+  let dp = [];
+  for (let i in a1) {
+    // console.log(`index: ${i} has value ${a1[i]}`);
+    console.log(`dp=${dp}`);
+    const v1 = a1[i];
+    const v2 = a2[i];
+    if (v1 !== v2) {
+      dp.push(i);
+    }
+  }
+  console.log(`returning ${dp}`);
+  return dp;
+};
+
 describe("Day 2 Problems", function() {
+  it("fghij and fguij diff example", function() {
+    expect([2, 3]).toEqual([2, 3]);
+    console.log(`[4]=${[4]}`);
+    let a = [];
+    a.push(4);
+    expect(a).toEqual([4]);
+    expect(diffPositions("fghij", "fguij")).toEqual([2]); ### WHY!!!!??
+  });
   it("Fequency example 1", function() {
     const s = `bababc`;
     expect(repeat3(s)).toBe(true);
