6f14ba448c1f153fe29778164d0f1e364ddea756
Matt Payne <Payne@MattPayne.org>
2018-12-02 18:20:07 -0600

    npm test # passes.  Time to clean up the mess.

---
+++ 1/spec/day1.spec.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var Day1 = require("../app/day1");
+
+describe("Day 1 problem", function() {
+  it("resulting frequency should be 466", function() {
+    Day1.resultingFrequency([1, 2, 3]);
+    let rf = null;
+    fs.readFile("input.txt", "utf8", function(err, data) {
+      if (err) throw err;
+      //console.log(data);
+      rf = Day1.resultingFrequency(data);
+      console.log(`\n\n\n\n\n rf=${rf}\n\n\n`);
+    });
+    expect(rf).toBe(466);
+  });
+});
